adee3596cf001529b73468cf3d31cef58d3f7441
SeungJooKim <seoungjoo01@gmail.com>
2021-07-05 14:30:49 +0900

    Unit10💛

---
+++ Python/CODE/Unit10.py
@@ -0,0 +1,16 @@
+a = [38, 21, 53, 33, 44]
+
+print(a)
+
+person = ['james', 17, 175.3, True]
+print(person)
+
+a = []
+
+b = list()
+
+range(10)
+
+c = list(range(-4, 10, 2))
+
+d = list(range(10, 0, -1))
