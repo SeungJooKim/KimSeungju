0d574e9b38f27af220a73031889872927e8e33c9
SeungJooKim <seoungjoo01@gmail.com>
2021-07-05 15:51:18 +0900

    Unit10❤

--- Python/CODE/Unit10.py
+++ Python/CODE/Unit10.py
@@ -14,3 +14,15 @@ range(10)
 c = list(range(-4, 10, 2))
 
 d = list(range(10, 0, -1))
+
+
+a = [1, 2, 3]
+tuple(a)  # 튜플은 변경 불가 #읽기 전용 리스트
+
+b = (4, 5, 6)
+list(b)
+
+
+x = [1, 2, 3]
+a, b, c = x
+print(a, b, c)  # 1 2 3
