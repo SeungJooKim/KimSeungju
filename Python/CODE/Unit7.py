c69251ce8f16923a1d3c0bc4ba26b7b9eadf5875
SeungJooKim <seoungjoo01@gmail.com>
2021-07-05 14:32:03 +0900

    Unit7💙

---
+++ Python/CODE/Unit7.py
@@ -0,0 +1,17 @@
+print(1, 2, 3)
+
+print(1, 2, 3, sep=', ')
+
+print(4, 5, 6, sep=',')
+
+print('Hello', 'Python', sep='')
+
+print(1920, 1080, sep='x')
+
+print(1, 2, 3, sep='\n')
+
+print('1\n2\n3')
+
+print(1, end=' ')   # end에 공백 지정
+print(2, end=' ')
+print(3)
