1be72e86052edd814d5bf32db78c9251bccc27eb
SeungJooKim <seoungjoo01@gmail.com>
2021-07-08 01:28:57 +0900

    8단계😊

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -4,9 +4,7 @@ class NotIn123(Exception):
         super().__init__('1,2,3 중에 선택해주세요')
 
 
-num = 0
-
-while(True):
+def brGame(player, num):
     while(True):
         try:
             player_num = int(input('부를 숫자의 개수를 입력하세요 (1,2,3 만 입력가능) : '))
@@ -23,28 +21,18 @@ while(True):
 
     for i in range(player_num):
         num = num+1
-        print('PlayerA : ', num)
-        if num == 31:
-            print('PlayerB win!')
-            exit()
+        print(player, ' : ', num)
+    return num
 
-    while True:
-        try:
-            player_num = int(input('부를 숫자의 개수를 입력하세요 (1,2,3 만 입력가능) : '))
-            if 1 > player_num or player_num > 3:
-                raise NotIn123
-            else:
-                break
 
-        except NotIn123 as e:
-            print(e)
+num = 0
 
-        except ValueError:
-            print('정수를 입력하세요')
-
-    for i in range(player_num):
-        num = num+1
-        print('PlayerB : ', num)
-        if num == 31:
-            print('PlayerA win!')
-            exit()
+while(True):
+    num = brGame("PlayerA", num)
+    if num == 31:
+        print('PlayerB win!')
+        break
+    num = brGame("PlayerB", num)
+    if num == 31:
+        print('PlayerA win!')
+        break
