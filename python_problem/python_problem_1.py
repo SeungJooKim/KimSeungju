429e59cb46494d5ef743ed33b3af365113ee0828
SeungJooKim <seoungjoo01@gmail.com>
2021-07-08 00:49:23 +0900

    3단계😊

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,21 @@
+# 예외 정의
+class NotIn123(Exception):
+    def __init__(self):
+        super().__init__('1,2,3 중에 선택해주세요')
+
+
 num = 0
 
-player_num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+while(True):
+    try:
+        player_num = int(input('부를 숫자의 개수를 입력하세요 (1,2,3 만 입력가능) : '))
+        if 1 > player_num or player_num > 3:
+            raise NotIn123
+        else:
+            break
+
+    except NotIn123 as e:
+        print(e)
+
+    except ValueError:
+        print('정수를 입력하세요')
