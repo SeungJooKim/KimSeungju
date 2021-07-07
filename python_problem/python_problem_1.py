634f66134496a2f1288749e72f587dcd1c931cf7
SeungJooKim <seoungjoo01@gmail.com>
2021-07-08 01:09:04 +0900

    5단계😊

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -23,3 +23,21 @@ while(True):
 for i in range(player_num):
     num = num+1
     print('PlayerA : ', num)
+
+while(True):
+    try:
+        player_num = int(input('부를 숫자의 개수를 입력하세요 (1,2,3 만 입력가능) : '))
+        if 1 > player_num or player_num > 3:
+            raise NotIn123
+        else:
+            break
+
+    except NotIn123 as e:
+        print(e)
+
+    except ValueError:
+        print('정수를 입력하세요')
+
+for i in range(player_num):
+    num = num+1
+    print('PlayerB : ', num)
