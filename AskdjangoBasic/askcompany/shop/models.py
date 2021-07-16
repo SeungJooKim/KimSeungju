994bbbcfbdd9d7d8c4d36a97d7b416a414c472cd
SeungJooKim <seoungjoo01@gmail.com>
2021-07-17 00:22:26 +0900

    Askdjango 9강📑

--- AskdjangoBasic/askcompany/shop/models.py
+++ AskdjangoBasic/askcompany/shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True)  # 공백도 ok
+    price = models.PositiveIntegerField()  # 양수
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
