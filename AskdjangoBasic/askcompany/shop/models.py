3d6d57023a01fb6cf7bd0a83801ffabb1285e5cc
SeungJooKim <seoungjoo01@gmail.com>
2021-07-17 01:52:03 +0900

    Askdjango 12강📑

--- AskdjangoBasic/askcompany/shop/models.py
+++ AskdjangoBasic/askcompany/shop/models.py
@@ -18,8 +18,12 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)  # 공백도 ok
     price = models.PositiveIntegerField()  # 양수
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'<{self.pk}> {self.name}'
 
 
 """
