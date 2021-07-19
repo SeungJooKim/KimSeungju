c32927bb46c0d75bc39109368f7023756f4a1304
SeungJooKim <seoungjoo01@gmail.com>
2021-07-20 06:28:20 +0900

    Askdjango 중급 7강💻

--- AskdjangoBasic/askdjango/accounts/views.py
+++ AskdjangoBasic/askdjango/accounts/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render,resolve_url
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.views.generic import CreateView
 from django.contrib.auth.models import User
 from .forms import SignupForm
+from django.contrib.auth import login as auth_login
 # def signup(request):
 #     if request.method =="POST":
 #         form = UserCreationForm(request.POST)
@@ -16,7 +17,22 @@ from .forms import SignupForm
 #     return render(request, 'accounts/signup.html',{
 #         'form': form,
 #     })
-signup =CreateView.as_view(model=User,form_class=SignupForm, template_name="accounts/signup.html", success_url =settings.LOGIN_URL)
+#signup =CreateView.as_view(model=User,form_class=SignupForm, template_name="accounts/signup.html", success_url =settings.LOGIN_URL)
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return resolve_url('profile')
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+
+signup = SignupView.as_view()
 
 @login_required
 def profile(request):
