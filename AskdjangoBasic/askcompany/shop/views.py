a2a98a06428ad8a4d65b9e5fb62e0c4fb559673b
SeungJooKim <seoungjoo01@gmail.com>
2021-07-20 02:52:55 +0900

    Askdjango 30강📑

--- AskdjangoBasic/askcompany/shop/views.py
+++ AskdjangoBasic/askcompany/shop/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_list_or_404, get_object_or_404, render,redirect
 from django.http import HttpResponse
 from .models import Item
 import re
+from .forms import  ItemForm 
+from django.views.generic import CreateView,UpdateView
 
 
 def archives_year(request, year):
@@ -44,67 +46,23 @@ def item_detail(request, pk):
 def item_new(request):
     return render(request,'shop/item_form.html')
 
-def item_new(request, item=None):
-    error_list = []
-    initial = {}
+# def item_new(request, item=None):
 
-    if request.method == 'POST':
-        data = request.POST
-        files = request.FILES
+#     if request.method == 'POST':
+#         form = ItemForm(request.POST, request.FILES,instance=item)
+#         if form.is_valid():
+#             item = form.save()
+#             return redire ct(item)
+#     else :
+#         form =ItemForm(instance=item)
 
-        name = data.get('name')
-        desc = data.get('desc')
-        price = data.get('price')
-        photo = files.get('photo')
-        is_publish = data.get('is_publish') in (True, 't', 'True', '1')
+#     return render(request, 'shop/item_form.html', {
+#         'form': form,
+#     })
 
-        if len(name) < 2:
-            error_list.append('name을 2글자 이상 입력해주세요.')
+# def item_edit(request, pk):
+#     item = get_object_or_404(Item, pk=pk)
+#     return item_new(request, item)
 
-        if re.match(r'^[\da-zA-Z\s]+$', desc): 
-            error_list.append('한글을 입력해주세요.')
-
-        if not error_list:
-            if item is None:
-                item = Item()
-
-            item.name = name
-            item.desc = desc
-            item.price = price
-            item.is_publish = is_publish
-
-            if photo:
-                item.photo.save(photo.name, photo, save=False)
-
-            try:
-                item.save()
-            except Exception as e:
-                error_list.append(e)
-            else:
-                return redirect(item) #item.get_absolute_url 호출
-
-        initial = {
-            'name': name,
-            'desc': desc,
-            'price': price,
-            'photo': photo,
-            'is_publish': is_publish,
-        }
-    else:
-        if item is not None:
-            initial = {
-                'name': item.name,
-                'desc': item.desc,
-                'price': item.price,
-                'photo': item.photo,
-                'is_publish': item.is_publish,
-            }
-
-    return render(request, 'shop/item_form.html', {
-        'error_list': error_list,
-        'initial': initial,
-    })
-
-def item_edit(request, pk):
-    item = get_object_or_404(Item, pk=pk)
-    return item_new(request, item)
+item_new = CreateView.as_view(model=Item, form_class=ItemForm)
+item_edit = UpdateView.as_view(model=Item,form_class=ItemForm)
