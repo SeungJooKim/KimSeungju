517179bb7d5583e809cb63ad9560d21555a645e9
SeungJooKim <seoungjoo01@gmail.com>
2021-07-20 03:14:55 +0900

    Askdjango 31강📑

--- AskdjangoBasic/askcompany/shop/urls.py
+++ AskdjangoBasic/askcompany/shop/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('<int:pk>/', views.item_detail,name='item_detail'),
     # re_path(r'^(?P<pk>\d+)\$',views.item_detail)
     path('new/', views.item_new, name='item_new'),
-    path('<int:pk>/edit/',views.item_edit, name='item_edit'),
+    path('<int:pk>/edit/',views.item_edit, name='item_edit'), 
 
 ]
