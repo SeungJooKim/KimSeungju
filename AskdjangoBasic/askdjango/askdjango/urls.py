88845a0488e8bfa8062fd9f651a85fe24f763c14
SeungJooKim <seoungjoo01@gmail.com>
2021-07-20 05:21:03 +0900

    Askdjango 중급 3강💻

--- AskdjangoBasic/askdjango/askdjango/urls.py
+++ AskdjangoBasic/askdjango/askdjango/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('blog/', include('blog.urls') ),
-    path('', lambda req : redirect('blog:post_list')), #url reverse를 쓰면 좋음
+    path('', lambda req : redirect('blog:post_list'),name='root'), #url reverse를 쓰면 좋음
 ]
